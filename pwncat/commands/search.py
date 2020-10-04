de70dd04b691d8d48d3d10d9db7c6e764e384400
Caleb Stewart <caleb.stewart94@gmail.com>
2020-10-04 18:00:58 -0400

    Fixed search command

--- pwncat/commands/search.py
+++ pwncat/commands/search.py
@@ -21,14 +21,14 @@ class Command(CommandDefinition):
     def run(self, args):
 
         table = Table(
-            Column(0, header="Name", ratio=0.2),
-            Column(1, header="Description", no_wrap=True, ratio=0.8),
+            Column(header="Name", ratio=0.2),
+            Column(header="Description", no_wrap=True, ratio=0.8),
             title="Results",
             box=box.MINIMAL_DOUBLE_HEAD,
             expand=True,
         )
 
-        for module in pwncat.modules.match(args.module):
+        for module in pwncat.modules.match(f"*{args.module}*"):
             # Rich will ellipsize the column, but we need to squeze
             # white space and remove newlines. `textwrap.shorten` is
             # the easiest way to do that, so we use a large size for
